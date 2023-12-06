71113a321b8dba039f562dd30cff4ad6d9056443
Dek Arya <arya.satyad123@gmail.com>
2023-12-06 14:41:19 +0800

    update API

--- components/Modal/Artikel/AddArtikel.tsx
+++ components/Modal/Artikel/AddArtikel.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 interface ArtikelModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onAddArtikel: (artikel: Artikel) => void;
+  onAddArtikel: (artikel: Artikel, image: File) => void;
 }
 
 const AddArtikelModal: React.FC<ArtikelModalProps> = ({
@@ -21,6 +21,9 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
     gambar: null,
   });
 
+  const [image, setImage] = useState<File | null>(null); // State to store the selected image
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -28,19 +31,49 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
       [name]: value,
     }));
   };
-
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // Access the first file from the input
-    const file = e.target.files?.[0];
-    setFormData((prevData) => ({
-      ...prevData,
-      gambar: file ? URL.createObjectURL(file) : null,
-    }));
+    const imageData = e.target.files?.[0];
+
+    if (!imageData?.type.match("image.*")) {
+      setImage(null);
+      return;
+    }
+
+    setImage(imageData);
   };
 
   const handleSubmit = () => {
+    const validationErrors: { [key: string]: string } = {};
+
+    // Perform form validation
+    if (formData.judul.trim() === "") {
+      validationErrors.judul = "Judul is required.";
+    }
+
+    if (formData.deskripsi.trim() === "") {
+      validationErrors.deskripsi = "Deskripsi is required.";
+    }
+
+    if (formData.pengurus_panti_id <= 0) {
+      validationErrors.pengurus_panti_id =
+        "Pengurus Panti ID must be greater than 0.";
+    }
+
+    if (!image) {
+      validationErrors.gambar = "Gambar is required.";
+    }
+
+    // Set validation errors and prevent form submission if there are errors
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    // Reset errors
+    setErrors({});
+
     // You can add validation here before submitting
-    onAddArtikel(formData);
+    onAddArtikel(formData, image as File);
     onClose();
   };
 
@@ -48,9 +81,9 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
     <div
       className={`${
         isOpen ? "block" : "hidden"
-      } fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center lg:ml-40 z-99`}
+      } fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center lg:ml-40 z-99  `}
     >
-      <div className="bg-white p-8 max-w-md w-full rounded-lg shadow-md border shadow-[#2D9596]">
+      <div className="bg-white p-8 max-w-md w-full rounded-lg shadow-md border shadow-[#2D9596] max-h-[80vh] overflow-y-auto overflow-hidden">
         <div className="text-xl font-bold mb-4 dark:text-black">
           Add Artikel
         </div>
@@ -67,6 +100,9 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
             onChange={handleInputChange}
             className="mt-1 p-2 w-full border rounded-md"
           />
+          {errors.judul && (
+            <p className="text-[#D80032] text-sm">{errors.judul}</p>
+          )}
         </div>
 
         <div className="mb-4">
@@ -80,6 +116,9 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
             onChange={handleInputChange}
             className="mt-1 p-2 w-full border rounded-md"
           />
+          {errors.deskripsi && (
+            <p className="text-[#D80032] text-sm">{errors.deskripsi}</p>
+          )}
         </div>
 
         <div className="mb-4">
@@ -93,6 +132,9 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
             onChange={handleInputChange}
             className="mt-1 p-2 w-full border rounded-md"
           />
+          {errors.pengurus_panti_id && (
+            <p className="text-[#D80032] text-sm">{errors.pengurus_panti_id}</p>
+          )}
         </div>
 
         {/* Add file input for gambar */}
@@ -113,12 +155,15 @@ const AddArtikelModal: React.FC<ArtikelModalProps> = ({
         {formData.gambar && (
           <div className="mb-4">
             <Image
-              width={500}
-              height={500}
+              width={150}
+              height={150}
               src={formData.gambar}
               alt="Gambar Artikel"
               className="max-w-full h-auto rounded-md"
             />
+            {errors.gambar && (
+              <p className="text-[#D80032] text-sm">{errors.gambar}</p>
+            )}
           </div>
         )}
 
