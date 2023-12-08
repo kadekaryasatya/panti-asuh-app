4e0ce468b69edbb1aa6656a008af925fe6c17357
Dek Arya <arya.satyad123@gmail.com>
2023-12-08 22:22:39 +0800

    Update Artikel and Detail

--- components/Artikel/Artikel.tsx
+++ components/Artikel/Artikel.tsx
@@ -1,50 +1,138 @@
+"use client";
+import { Artikel } from "@/types/artikel";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import Link from "next/link";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Artikel = () => {
+  const [artikel, setArtikelData] = useState<Artikel[]>([]);
+  const [pagination, setPagination] = useState({
+    total: 0,
+    per_page: 5,
+    current_page: 1,
+    last_page: 1,
+    from: 1,
+    to: 1,
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [loading, setLoading] = useState(true); // Add loading state
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async (page = 1) => {
+    try {
+      const authToken = Cookies.get("auth_token");
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BACKEND}/api/artikel?page=${page}`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setArtikelData(response.data.data);
+      setPagination(response.data.pagination);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false); // Set loading to false once data is fetched
+    }
+  };
+
+  const handlePageChange = (page: number) => {
+    fetchData(page);
+  };
   return (
     <div className="lg:px-30 px-4 py-8 lg:py-16 text-background2 bg-white">
-      <h1 className="lg:text-4xl font-bold  mb-10">Artikel Terbaru</h1>
-      <div className="flex gap-10">
-        <a
-          href="#"
-          className="flex flex-col items-center w-full bg-white  border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-        >
-          <Image
-            width={300}
-            height={300}
-            src={"/images/landing-page/artikel1.jpg"}
-            alt="Logo"
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Anak Berprestasi
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Anak anak panti asuhan yang berprestasi 2023
-            </p>
-          </div>
-        </a>
+      <h1 className="lg:text-4xl  text-3xl font-bold  mb-10 ">List Artikel</h1>
+      <div className="">
+        <div className="grid gap-x-4 gap-y-4 lg:grid-cols-3 md:grid-cols-2">
+          {loading ? ( // Check loading state
+            // Display skeleton cards while loading
+            [...Array(6)].map((_, index) => (
+              <div key={index} className="">
+                <Skeleton height={200} width="100%" />
+                <div className="flex flex-col justify-between leading-normal w-full">
+                  <Skeleton height={16} width={100} />
+                  <Skeleton height={24} width={200} />
+                  <Skeleton height={60} width="100%" />
+                </div>
+              </div>
+            ))
+          ) : artikel.length > 0 ? (
+            artikel.map((artikelItem, key) => (
+              <Link
+                key={key}
+                href={`/artikel/${artikelItem.id}`}
+                className="flex flex-col items-center w-full  bg-white border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <Image
+                  width={300}
+                  height={300}
+                  src={`${process.env.NEXT_PUBLIC_API_BACKEND}/storage/artikel/${artikelItem.gambar}`}
+                  alt="Logo"
+                  className="h-[200px] w-full object-cover rounded-t-lg"
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal w-full">
+                  <p className="text-xs">
+                    Admin,
+                    <span className="ml-1">
+                      {artikelItem.created_at
+                        ? new Date(artikelItem.created_at).toLocaleDateString(
+                            "id-ID",
+                            {
+                              day: "numeric",
+                              month: "long",
+                              year: "numeric",
+                            }
+                          )
+                        : "No date available"}
+                    </span>
+                  </p>
 
-        <a
-          href="#"
-          className="flex flex-col items-center w-full bg-white  border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-        >
-          <Image
-            width={300}
-            height={300}
-            src={"/images/landing-page/artikel2.jpg"}
-            alt="Logo"
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Cara membayar zakat yang baik
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Panduan cara membayar zakat yang baik dan benar menurut UUD
-            </p>
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white text-[#23549e] hover:text-background2">
+                    {artikelItem.judul}
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
+                    {artikelItem.deskripsi}
+                  </p>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p className="font-medium text-black dark:text-white">No Artikel</p>
+          )}
+        </div>
+        {pagination.total > pagination.per_page && (
+          <div className="flex justify-center mt-4">
+            <ul className="flex gap-1">
+              {[...Array(pagination.last_page).keys()].map((page) => (
+                <li key={page + 1}>
+                  <button
+                    onClick={() => handlePageChange(page + 1)}
+                    className={`px-3 py-2 rounded-md ${
+                      pagination.current_page === page + 1
+                        ? "bg-background2 text-white border"
+                        : "bg-white border "
+                    }`}
+                  >
+                    {page + 1}
+                  </button>
+                </li>
+              ))}
+            </ul>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
---
+++ components/Artikel/DetailArtikel.tsx
@@ -0,0 +1,76 @@
+// DetailArtikel.tsx
+"use client";
+import { Artikel } from "@/types/artikel";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import Image from "next/image";
+
+import { Metadata } from "next";
+import { useParams } from "next/navigation";
+
+export const DetailArtikel = () => {
+  const [artikel, setArtikelData] = useState<Artikel | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      fetchArtikelDetails(id as string);
+    }
+  }, [id]);
+
+  const fetchArtikelDetails = async (articleId: string) => {
+    try {
+      const authToken = Cookies.get("auth_token");
+
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BACKEND}/api/artikel/${articleId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setArtikelData(response.data);
+
+      // Dynamically set the title based on artikel.judul
+      document.title = `${response.data.judul} - Panti Asuhan App`;
+    } catch (error) {
+      console.error("Error fetching artikel details:", error);
+    }
+  };
+
+  if (!artikel) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="lg:px-35 px-4 py-8 lg:py-16 text-background2 bg-white">
+      <h1 className="lg:text-4xl  text-3xl font-bold mb-2">{artikel.judul}</h1>
+      <div className="flex flex-col justify-between leading-normal w-full mb-5">
+        <p className="">
+          Admin,
+          <span className="ml-1">
+            {artikel.created_at
+              ? new Date(artikel.created_at).toLocaleDateString("id-ID", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })
+              : "No date available"}
+          </span>
+        </p>
+      </div>
+      <Image
+        width={300}
+        height={300}
+        src={`${process.env.NEXT_PUBLIC_API_BACKEND}/storage/artikel/${artikel.gambar}`}
+        alt="Logo"
+        className="h-[500px] w-full object-cover rounded-t-lg"
+      />
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 mt-5">
+        {artikel.deskripsi}
+      </p>
+    </div>
+  );
+};
